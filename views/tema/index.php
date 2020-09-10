6bf589f69e3b26e0edd0782acec8275b9409a521
Arwan <arwanpriantomangidi@gmail.com>
2020-09-11 00:02:19 +0800

    tambah-view-tema

---
+++ views/tema/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\TemaCari */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Temas';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tema-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Tema', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'tema_id',
+            'tema_nama',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
