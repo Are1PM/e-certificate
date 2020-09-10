9e0d264cc63de8020ba5be04f7345bbed06aa9ca
Arwan <arwanpriantomangidi@gmail.com>
2020-09-11 00:01:01 +0800

    tambah-model-cari-tema

---
+++ models/TemaCari.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Tema;
+
+/**
+ * TemaCari represents the model behind the search form of `app\models\Tema`.
+ */
+class TemaCari extends Tema
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['tema_id'], 'integer'],
+            [['tema_nama'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Tema::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'tema_id' => $this->tema_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'tema_nama', $this->tema_nama]);
+
+        return $dataProvider;
+    }
+}
